14f097728df13894c4e69a6b01c9c724506f0f6f
Vedant Sanil <vsanil@workhorse.lti.cs.cmu.edu>
2020-03-25 20:51:40 -0400

    Updated scripts for date-wise result generation

--- lazygit.sh
+++ lazygit.sh
@@ -2,5 +2,5 @@
 python3 modify_html.py
 git init
 git add .
-git commit -m 'Updated scripts for date-wise result generation'
+git commit -m "$1"
 git push "https://$2:$3@github.com/vedant-sanil/cvd.github.io.git"
